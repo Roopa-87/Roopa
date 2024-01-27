de4c95156ac24e95dc0ba5aa941af596f7a11a50
Roopa-87 <155879788+Roopa-87@users.noreply.github.com>
2024-01-27 12:03:57 +0530

    Add files via upload

---
+++ _inheri.py
@@ -0,0 +1,17 @@
+class Parent:
+    a=10
+    b=20
+    def __init__(self,c,d):
+        self.c=c
+        self.d=d
+class Child(Parent):
+  a=30
+  def __init__(self,c,d,e,f):
+      super().__init__(c,d)
+      self.e=e
+      self.f=f  
+  def display(self):
+      #parent.display(self)
+      super().display()
+      print(self,e,self,f)
+obj=Child(4,5,6,7)
