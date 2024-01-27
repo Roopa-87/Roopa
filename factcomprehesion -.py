33e5bd5e5ca8904e10c0aff1aa7508ce1e12e468
Roopa-87 <155879788+Roopa-87@users.noreply.github.com>
2024-01-27 13:55:30 +0530

    Add files via upload

---
+++ factcomprehesion -.py
@@ -0,0 +1,5 @@
+def fact(num):
+    out=1
+    for _ in range(1,num):
+         out*=_
+    return out
---
+++ file.py
@@ -0,0 +1,4 @@
+file=open('sample.txt','w')
+data='Happy Republic day'
+file.write(data)
+file.close()
